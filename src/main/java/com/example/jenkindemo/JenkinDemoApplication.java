cf64f74bf01eeefabfd82f44dfec20b61a1ebf92
sonicercli <soni@cercli.com>
2023-07-07 16:32:29 +0400

    first commit

---
+++ src/main/java/com/example/jenkindemo/JenkinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinDemoApplication.class, args);
+    }
+
+}
